62ae5c3ac51f8fd824fd1e4ba09ac3a7bfa88716
Aryank200010013 <77934096+Aryank200010013@users.noreply.github.com>
2021-01-25 00:55:55 +0530

    Update Normal equation.py

--- Python_Training_Codes/Normal equation.py
+++ Python_Training_Codes/Normal equation.py
@@ -1,31 +1,19 @@
 import numpy as np
 
-
-
 x=np.random.normal(0,3.14,size=(20,20))
 
 y=np.arange(20,dtype=np.int32)
-
-
 
 y=y.reshape(20,1)
 
 xt=np.matrix.transpose(x)
 
-
 z=x*xt
-
 
 zi=np.linalg.inv(z)
 
-
-
 z2=zi*xt
 
-
 z_final=z2.dot(y)
-
-
-
 
 print(z_final)
